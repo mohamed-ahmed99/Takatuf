90dad5a42729e742f5cde66cbaa722b73d1f8a5d
YourName <youremail@example.com>
2026-05-31 04:00:33 +0300

    the based arch

---
+++ backend/app.js
@@ -0,0 +1,32 @@
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import cors from "cors";
+import { connectDB } from "./config/db.js";
+import { errorHandler } from "./middlewares/errorMiddleware.js";
+import authrouter from "./routes/auth.router.js"
+//
+const app = express();
+//
+dotenv.config();
+app.use(express.json());
+app.use(cors({
+  origin: /^http:\/\/localhost:\d+$/,  // ← يقبل أي localhost
+  credentials: true
+}));
+
+
+// db
+connectDB();
+
+// use routes
+app.use("/api", authrouter);
+app.use(errorHandler);
+
+//
+const port = process.env.PORT || 5000;
+app.listen(port, () => {
+  console.log(`server is running on ${port}`);
+});
+
+
