180679292ef06ea290775d034cb0a65acaea0692
YourName <youremail@example.com>
2026-06-06 18:04:18 +0300

    testmiddleware

--- back-end/src/controllers/user.controller.js
+++ back-end/src/controllers/user.controller.js
@@ -229,4 +229,3 @@ export const partialUpdateUser = asyncHandler(async (req, res) => {
 
 
 
-
---
+++ back-end/src/middlewares/auth.middleware.js
@@ -0,0 +1,44 @@
+import jwt from "jsonwebtoken";
+import asyncHandler from "express-async-handler";
+
+export const protect = asyncHandler(async (req, res, next) => {
+    const token = req.cookies?.token;
+
+    if (!token) {
+        return res.status(401).json({
+            status: "fail",
+            message: "Not authorized, no token",
+            data: null
+        });
+    }
+
+    try {
+        const decoded = jwt.verify(
+            token,
+            process.env.JWT_SECRET
+        );
+
+        req.user = decoded;
+
+        next();
+    } catch (error) {
+        return res.status(401).json({
+            status: "fail",
+            message: "Invalid or expired token",
+            data: null
+        });
+    }
+});
+
+export const adminOnly = (req, res, next) => {
+    if (req.user.role !== "admin") {
+        return res.status(403).json({
+            status: "fail",
+            message: "Access denied",
+            data: null
+        });
+    }
+
+    next();
+};
+
